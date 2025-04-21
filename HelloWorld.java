019f99beb21454fb0fb9a643cfc618cd660e1772
Ruchhira Devane <ruchiradevanejnec@gmail.com>
2025-04-21 15:27:54 +0530

    push

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! My first pipeline is working. Test");
+        System.out.println("Hello, Jenkins! My first pipeline is working. ");
+        System.out.println("Hello, Jenkins!. ");
+
     }
 }
